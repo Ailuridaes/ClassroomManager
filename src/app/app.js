c521a8a9b93e1a4b3de0a899567653b86bb874eb
Ailuridaes <ailuridaes@gmail.com>
2016-09-07 13:33:18 -0700

    Setup routing

--- src/app/app.js
+++ src/app/app.js
@@ -2,5 +2,50 @@
     'use strict';
 
     angular
-        .module('app', ['ui.router']);
+        .module('app', ['ui.router'])
+        .config(appConfig);
+
+        appConfig.$inject = ["$urlRouterProvider", "$stateProvider"];
+
+        function appConfig($urlRouterProvider, $stateProvider) {
+            $urlRouterProvider.otherwise('dashboard');
+
+            $stateProvider.state('dashboard', {
+                url: '/dashboard',
+                controller: 'DashboardController as dashboard',
+                templateUrl: 'app/dashboard/dashboard.html'
+            });
+
+            $stateProvider.state('student', {
+                url: '/student',
+                abstract: true,
+                template: '<div ui-view></div>'
+            })
+                .state('student.list', {
+                    url: '/list',
+                    controller: 'StudentListController as studentList',
+                    templateUrl: 'app/student/student.list.html'
+                })
+                .state('student.detail', {
+                    url: '/:studentId',
+                    controller: 'StudentDetailController as studentDetail',
+                    templateUrl: 'app/student/student.detail.html'
+                });
+
+            $stateProvider.state('project', {
+                url: '/project',
+                abstract: true,
+                template: '<div ui-view></div>'
+            })
+                .state('project.list', {
+                    url: '/list',
+                    controller: 'ProjectListController as projectList',
+                    templateUrl: 'app/project/project.list.html'
+                })
+                .state('project.detail', {
+                    url: '/:projectId',
+                    controller: 'ProjectDetailController as projectDetail',
+                    templateUrl: 'app/project/project.detail.html'
+                });
+        }
 })();
